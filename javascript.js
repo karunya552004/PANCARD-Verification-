4d797f1def76c9b5d51236086983eb09a23547a8
Karunya Anandakumar <karunya050504@gmail.com>
2024-07-30 17:15:33 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function ValidatePAN() {
+    var txtPANCard = document.getElementById("txtPANCard");
+    var lblPANCard = document.getElementById("lblPANCard")
+    var regex = /([A-Z]){5}([0-9]){4}([A-Z]){1}$/;
+    if (regex.test(txtPANCard.value.toUpperCase())) {
+        alert("registered successfully")
+        return true;
+    } else {
+        lblPANCard.style.visibility = "visible";
+        return false;
+    }
+}
